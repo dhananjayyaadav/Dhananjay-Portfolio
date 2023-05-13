b88a9ffa45a283b4bbd56aeb0190263411e982ad
Dhananjay Yadav <110413258+dhananjayyaadav@users.noreply.github.com>
2023-05-14 05:01:28 +0530

    Update portfolio.ts

--- portfolio.ts
+++ portfolio.ts
@@ -229,16 +229,13 @@ export const projects: ProjectType[] = [
   },
   {
     name: 'MyMeta Wallet',
-    desc: 'Build a MyMeta Wallet Blockchain Project It is a Crypto Wallet that provides Easy Access to store and share Crypto Coins or Digital Assets.
-          'A MyMeta Wallet is a Crypto Wallet. Which is based on Web 3.O, It is Built using Blockchain Technology.',
+    desc: 'Build a MyMeta Wallet Blockchain Project It is a Crypto Wallet that provides Easy Access to store and share Crypto Coins or Digital Assets.',
          github: 'https://github.com/dhananjayyaadav/My-Meta-Wallet',
   },
   {
     name: 'MATEBOOK |Full Stack Development',
     desc: 'Build a Matebook App using React, CSS, Express, and TypeScript.
-          'A Social Media web Application used for users to Post Photos along with description. Users can Send/Accept/Cancel Friend requests. Users can View/Like/Comment Friend’s Posts.',
      github: 'https://github.com/dhananjayyaadav/MateBook',
-     link: 'https://facebook-clone-dev.vercel.app/',
   },
 ];
 
