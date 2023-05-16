413b5c7e3300db1e68ee96516ba9e00d6da7c391
Dhananjay Yadav <110413258+dhananjayyaadav@users.noreply.github.com>
2023-05-17 00:18:27 +0530

    Update portfolio.ts

--- portfolio.ts
+++ portfolio.ts
@@ -17,7 +17,7 @@ export const greetings: GreetingsType = {
   description:
     "I'm passionate Data Science and Full Stack web developer having an experience of web applications with Python, Django, React.js, JavaScript, Node.js, Next.js, MongoDB and Blockchain development on Ethereum, Solidity, Web3.js.",
   resumeLink:
-    'https://drive.google.com/file/d/1Y7YXvNWP9pgFenXsAGcyfMoKmEtvbGTT/view?usp=drivesdk',
+    'https://drive.google.com/file/d/1gU43FZs-2f_1KcJbx6Y3EfM-EvVSQLef/view',
 };
 
 export const openSource = {
